0041f60307a4682c3f4d5963fc35a3c39f373007
enigma-vh <vanhao2818@gmail.com>
2022-02-12 20:37:44 -0800

    Updates

---
+++ app/src/main/java/com/example/fitshare/Recipe/RecipeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fitshare.Recipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitshare.R
+
+
+class RecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe)
+    }
+}
---
+++ app/src/main/java/com/example/fitshare/User/User.kt
@@ -0,0 +1,15 @@
+package com.example.fitshare.User
+
+import com.example.fitshare.Recipe.Recipe
+import io.realm.RealmList
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmField
+
+
+open class User(
+    @PrimaryKey @RealmField("_id") var id: String = "",
+    var _partition: String = "",
+    var recipes: RealmList<Recipe> ?= null,
+    var name: String = ""
+): RealmObject()
