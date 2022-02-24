4a50d3c80176ce48a8efa9a7e3eccc8cff2bf8d5
Bryann12349 <bryan.nguyen12349@gmail.com>
2022-02-23 19:12:49 -0800

    Cleaned up project activity

--- app/src/main/java/com/mongodb/tasktracker/ProjectActivity.kt
+++ app/src/main/java/com/mongodb/tasktracker/ProjectActivity.kt
@@ -89,10 +89,6 @@ class ProjectActivity : AppCompatActivity() {
                 }
                 true
             }
-//            R.id.action_add_recipe -> {
-//                startActivity(Intent(this, RecipeActivity::class.java))
-//                true
-//            }
 
             else -> {
                 super.onOptionsItemSelected(item)
