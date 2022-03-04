ddf67a4612462290515c72b17947a1d0cecc0b13
Bryann12349 <bryan.nguyen12349@gmail.com>
2022-03-03 21:47:59 -0800

    Profile Model

---
+++ app/src/main/java/Profile/Profile.kt
@@ -0,0 +1,29 @@
+package Profile
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmField
+import io.realm.annotations.Required
+import org.bson.types.ObjectId
+
+open class Profile (_fname: String = "First Name", _lname: String = "Last Name",
+                    _bio: String = "Bio", _addr: String = "Address",
+                    _zip: String = "Zipcode", _phone: String = "Phone Number",
+                    _meet: Boolean = false) : RealmObject(){
+    @PrimaryKey @RealmField ("_id") var id: ObjectId = ObjectId()
+    @Required
+    var firstName: String = _fname
+    @Required
+    var lastName: String = _lname
+    @Required
+    var bio: String = _bio
+    @Required
+    var address: String = _addr
+    @Required
+    var zipcode: String = _zip
+    @Required
+    var phoneNumber: String = _phone
+    @Required
+    var meetUp: Boolean = _meet
+
+}
