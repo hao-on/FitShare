3b00de19d3f2888992cad260c264d6d58c3961c3
cb3lito <80925653+cb3lito@users.noreply.github.com>
2022-03-10 18:51:16 -0800

    commits

--- app/src/main/java/com/example/fitshare/Fitness/Plan_Pull.kt
+++ app/src/main/java/com/example/fitshare/Fitness/Plan_Pull.kt
@@ -6,13 +6,13 @@ import io.realm.annotations.RealmField
 import io.realm.annotations.Required
 import org.bson.types.ObjectId
 
-open class Plan_Pull(name: String = "", _arms1: String = "", _arms1reps: Int = 0, _arms1sets: Int = 0,_arms2: String = "", _arms2reps: Int = 0, _arms2sets: Int = 0,
+open class Plan_Pull(_name: String = "", _arms1: String = "", _arms1reps: Int = 0, _arms1sets: Int = 0,_arms2: String = "", _arms2reps: Int = 0, _arms2sets: Int = 0,
                      _arms3: String = "", _arms3reps: Int = 0, _arms3sets: Int = 0,_back1 : String ="", _back1reps : Int = 0,
                      _back1sets : Int = 0,_back2 : String ="", _back2reps : Int = 0, _back2sets : Int = 0,
                      _back3 : String ="", _back3reps : Int = 0, _back3sets : Int = 0) : RealmObject() {
     @PrimaryKey @RealmField ("_id") var id: ObjectId = ObjectId()
     @Required
-    var _name: String = name
+    var name: String = _name
     var arms1: String = _arms1
     var arms1reps: Int = _arms1reps
     var arms1sets: Int = _arms1sets
