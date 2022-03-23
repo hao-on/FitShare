06824879640dc6adff70890dbef1f5d30289f9b6
cb3lito <80925653+cb3lito@users.noreply.github.com>
2022-03-22 19:58:02 -0700

    new commit

---
+++ app/src/main/java/com/example/fitshare/DiaryActivity.kt
@@ -0,0 +1,28 @@
+package com.example.fitshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.app.SearchManager
+import android.content.Context
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fitshare.R
+import com.example.fitshare.fitApp
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import io.realm.Case
+import io.realm.Realm
+import io.realm.kotlin.where
+import io.realm.mongodb.User
+import io.realm.mongodb.sync.SyncConfiguration
+class DiaryActivity : AppCompatActivity (){
+
+
+}
