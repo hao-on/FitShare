f1914643b40ba697bdab3e3c2072913966515238
Bryann12349 <bryan.nguyen12349@gmail.com>
2022-04-06 00:05:37 -0700

    Squashed commit of the following:
    commit c84fc4429d1d0454bb2c33a9a0744de23263abdf
    Author: enigma-vh <vanhao2818@gmail.com>
    Date:   Tue Apr 5 23:45:00 2022 -0700
    
        Updates v2.0
    
        - Profile UI
        - WorkOutPlan + Shopping Cart
        - Recipe Details
    
    commit 93f62eface57854fd8a4bbae7570de7dd2fed86d
    Author: Bryann12349 <bryan.nguyen12349@gmail.com>
    Date:   Thu Mar 31 15:35:59 2022 -0700
    
        re added drawables

--- app/src/main/java/com/example/fitshare/OtherProfileFragment.kt
+++ app/src/main/java/com/example/fitshare/OtherProfileFragment.kt
@@ -1,25 +1,15 @@
 package com.example.fitshare
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import com.example.fitshare.Profile.Profile
-import com.example.fitshare.Profile.ProfileEditButton
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import io.realm.Realm
 import io.realm.mongodb.sync.SyncConfiguration
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class OtherProfileFragment: Fragment() {
     private var user: io.realm.mongodb.User? = null
@@ -28,11 +18,7 @@ class OtherProfileFragment: Fragment() {
     private lateinit var partition: String
     private lateinit var messaging: AppCompatButton
     private lateinit var meetUp: CheckBox
-    private lateinit var profileName : TextView
-    private lateinit var username : TextView
-    private lateinit var phone : TextView
-    private lateinit var address : TextView
-    private lateinit var bio : TextView
+
 
 
     override fun onCreateView(
@@ -40,55 +26,34 @@ class OtherProfileFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view: View = inflater.inflate(R.layout.fragment_other_profile, container, false)
-        user = fitApp.currentUser()
-        partition = "Profile"
-        val config = SyncConfiguration.Builder(user!!, partition).build()
+        val view: View = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        Realm.getInstanceAsync(config, object: Realm.Callback(){
-            override fun onSuccess(realm: Realm) {
-                this@OtherProfileFragment.profileRealm = realm
-                //get passed profile userid data
-                var profileUserId = arguments?.getString("profileUserId")
-
-                val oldProf = profileRealm.where(Profile::class.java).
-                equalTo("userid", profileUserId).findFirst()
-
-                profileName = view.findViewById(R.id.profileName)
-                username = view.findViewById(R.id.username)
-                phone = view.findViewById(R.id.phone)
-                address = view.findViewById(R.id.address)
-                bio = view.findViewById(R.id.bio)
-                meetUp = view.findViewById(R.id.meetUp)
-
-                if(oldProf?.meetUp == true){
-                    meetUp.isChecked = true
-                }else{meetUp.isChecked = false}
-
-                username.setText(oldProf?.username)
-                profileName.setText(oldProf?.firstName + ", " + oldProf?.lastName)
-                phone.setText(oldProf?.phoneNumber)
-                address.setText(oldProf?.address + ", " + oldProf?.zipcode)
-                bio.setText(oldProf?.bio)
-            }
-        })
-
-        val user_config: SyncConfiguration =
-            SyncConfiguration.Builder(user!!, "user=${user!!.id}")
-                .build()
-        Realm.getInstanceAsync(user_config, object: Realm.Callback(){
-            override fun onSuccess(realm: Realm) {
-                this@OtherProfileFragment.userRealm = realm
-            }
-        })
-
-
-
-
+//        user = fitApp.currentUser()
+//        partition = "Profile"
+//        val config = SyncConfiguration.Builder(user!!, partition).build()
+//
+//        Realm.getInstanceAsync(config, object: Realm.Callback(){
+//            override fun onSuccess(realm: Realm) {
+//                this@OtherProfileFragment.profileRealm = realm
+//                val oldProf = profileRealm.where(Profile::class.java).
+//                equalTo("userid", user?.id.toString()).findFirst()
+//                if(oldProf?.meetUp == true){
+//                    meetUp.isChecked = true
+//                }else{meetUp.isChecked = false}
+//            }
+//
+//        })
+//
+//        val user_config: SyncConfiguration =
+//            SyncConfiguration.Builder(user!!, "user=${user!!.id}")
+//                .build()
+//        Realm.getInstanceAsync(user_config, object: Realm.Callback(){
+//            override fun onSuccess(realm: Realm) {
+//                this@OtherProfileFragment.userRealm = realm
+//            }
+//        })
         return view
     }
-
-
 
     override fun onDestroy() {
         super.onDestroy()
@@ -97,23 +62,4 @@ class OtherProfileFragment: Fragment() {
         profileRealm.close()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
